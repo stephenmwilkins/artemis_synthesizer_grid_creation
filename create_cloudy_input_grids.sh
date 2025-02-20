da98a798290bc922ac334fc67a698326405e7c51
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2025-02-20 13:10:15 +0000

    scripts

---
+++ create_cloudy_input_grids.sh
@@ -0,0 +1,33 @@
+# Runs the grid setup script for a series of grids and configurations.
+# This should create slurm job script that can be simply submitted to the 
+# queue.
+
+grid_dir='/research/astrodata/highz/synthesizer/grids'
+output_dir='/its/home/sw376/astrodata/gaslight/cloudy'
+cloudy_output_dir='/research/astro/flare/software/cloudy/'
+cloudy_executable_path='/its/home/sw376/flare/software/cloudy'
+machine='artemis'
+
+# list of incident grids
+incident_grids=("test")
+
+# list of config files
+config_files=("c23.01-sps")
+
+# change directory
+cd ..
+
+for i in "${!incident_grids[@]}"; do
+
+    incident_grid=${incident_grids[$i]}
+    config_file=${config_files[$i]}
+
+    python create_cloudy_input_grid.py \
+        --incident-grid=$incident_grid \
+        --grid-dir=$grid_dir \
+        --cloudy-output-dir=$cloudy_output_dir \
+        --cloudy-paramfile=c23.01-sps \
+        --machine=$machine \
+        --cloudy-executable-path=$cloudy_executable_path
+
+done
---
+++ create_synthesizer_grids.sh
@@ -0,0 +1,29 @@
+# Runs the synthesizer grid creation code.
+
+grid_dir='/research/astrodata/highz/synthesizer/grids'
+output_dir='/its/home/sw376/astrodata/gaslight/cloudy'
+cloudy_output_dir='/research/astro/flare/software/cloudy/'
+cloudy_executable_path='/its/home/sw376/flare/software/cloudy'
+machine='artemis'
+
+# list of incident grids
+incident_grids=("test")
+
+# list of config files
+config_files=("c23.01-sps")
+
+# change directory
+cd ..
+
+for i in "${!incident_grids[@]}"; do
+
+    incident_grid=${incident_grids[$i]}
+    config_file=${config_files[$i]}
+
+    python create_synthesizer_grid.py \
+    --incident-grid=$incident_grid \
+    --grid-dir=$grid_dir \
+    --cloudy-output-dir=$cloudy_output_dir \
+    --cloudy-paramfile=$config_file \
+
+done
