630ef5a38f835a571415a09cdaf8631c4959a48a
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2025-02-21 08:49:01 +0000

    bugfix

--- create_synthesizer_test_grids.sh
+++ create_synthesizer_test_grids.sh
@@ -46,6 +46,6 @@ for i in "${!config_files[@]}"; do
     --grid-dir=$grid_dir \
     --cloudy-output-dir=$cloudy_output_dir \
     --cloudy-paramfile=$default_config_file \
-    --cloudy-paramfile-extra=$config_file \
+    --cloudy-paramfile-extra=test_suite/$config_file \
 
 done
