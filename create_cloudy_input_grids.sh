e7d3043d8e062148e4ae172bdbc4df51600178e3
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2025-03-06 16:42:01 +0000

    bugfix

--- create_cloudy_input_grids.sh
+++ create_cloudy_input_grids.sh
@@ -10,7 +10,7 @@ machine='artemis'
 # list of incident grids
 incident_grids=("bpass-2.2.1-bin_chabrier03-0.1,300.0")
 # incident_grids=("bpass-2.2.1-bin_chabrier03-0.1,300.0-new_ages:6,7,8")
-incident_grids=("qsosed-limited")
+incident_grids=("qsosed-test" "relqso-test")
 
 # list of config files
 config_files=("c23.01-sps")
