6848df6a7201af9e2cebf0cfdcb57f98b788db0c
stephenmwilkins <ste.wilkins@gmail.com>
2025-09-21 16:55:36 +0100

    updated

--- create_cloudy_input_grids.sh
+++ create_cloudy_input_grids.sh
@@ -36,16 +36,16 @@ incident_grids=(
     # bpass-2.2.1-bin_chabrier03-0.1,100.0
     # bpass-2.2.1-bin_chabrier03-0.1,300.0
     # bpass-2.2.1-sin_bpl-0.1,1.0,100.0-1.3,2.0
-    bpass-2.2.1-sin_bpl-0.1,1.0,100.0-1.3,2.35
-    bpass-2.2.1-sin_bpl-0.1,1.0,100.0-1.3,2.7
-    bpass-2.2.1-sin_bpl-0.1,1.0,300.0-1.3,2.0
-    bpass-2.2.1-sin_bpl-0.1,1.0,300.0-1.3,2.35
-    bpass-2.2.1-sin_bpl-0.1,1.0,300.0-1.3,2.7
-    bpass-2.2.1-sin_chabrier03-0.1,100.0
-    bpass-2.2.1-sin_chabrier03-0.1,300.0
-    bpass-2.3-bin_bpl-0.1,1.0,300.0-1.3,2.35
-    bpass-2.3-bin_bpl-0.1,1.0,300.0-1.3,2.35_alpha-0.2
-    bpass-2.3-bin_bpl-0.1,1.0,300.0-1.3,2.35_alpha0.0
+    # bpass-2.2.1-sin_bpl-0.1,1.0,100.0-1.3,2.35
+    # bpass-2.2.1-sin_bpl-0.1,1.0,100.0-1.3,2.7
+    # bpass-2.2.1-sin_bpl-0.1,1.0,300.0-1.3,2.0
+    # bpass-2.2.1-sin_bpl-0.1,1.0,300.0-1.3,2.35
+    # bpass-2.2.1-sin_bpl-0.1,1.0,300.0-1.3,2.7
+    # bpass-2.2.1-sin_chabrier03-0.1,100.0
+    # bpass-2.2.1-sin_chabrier03-0.1,300.0
+    # bpass-2.3-bin_bpl-0.1,1.0,300.0-1.3,2.35
+    # bpass-2.3-bin_bpl-0.1,1.0,300.0-1.3,2.35_alpha-0.2
+    # bpass-2.3-bin_bpl-0.1,1.0,300.0-1.3,2.35_alpha0.0
     bpass-2.3-bin_bpl-0.1,1.0,300.0-1.3,2.35_alpha0.2
     bpass-2.3-bin_bpl-0.1,1.0,300.0-1.3,2.35_alpha0.4
     bpass-2.3-bin_bpl-0.1,1.0,300.0-1.3,2.35_alpha0.6
