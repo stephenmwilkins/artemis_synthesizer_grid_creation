c198554d25ad02fa848b5811583a4075c811e5ff
stephenmwilkins <ste.wilkins@gmail.com>
2025-10-07 08:32:52 +0100

    updated

--- create_cloudy_input_grids.sh
+++ create_cloudy_input_grids.sh
@@ -93,6 +93,10 @@ incident_grids=("bpass-2.2.1-bin_chabrier03-0.1,300.0_reduced_ages_[1.00e+06,1.0
 config_files=("c23.01-sps-grid")
 
 
+incident_grids=("qsosed-test" "qsosed-test")
+config_files=("c23.01-nlr-test" "c23.01-blr-test")
+
+
 # change directory
 cd ..
 
